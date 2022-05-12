684c3adfcd8b90cb343c0ea03e5195caecc72616
matheus.santos <matheus.santos@toctec.eng.br>
2022-05-12 00:56:45 -0300

    Chore: Commit inicial dos frameworks

---
+++ backend/LupitBackEnd/LupitBackEnd/Controllers/TimesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LupitBackEnd.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TimesController : ControllerBase
+    {
+       
+
+       
+
+      
+
+
+    }
+}

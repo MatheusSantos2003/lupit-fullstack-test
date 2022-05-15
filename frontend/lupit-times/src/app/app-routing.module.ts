9c52689c6c18ae8c387571d77dae7d688703feb0
matheus.santos <matheus.santos@toctec.eng.br>
2022-05-15 13:36:29 -0300

    chore:frontend: criação de base da aplicação

--- frontend/lupit-times/src/app/app-routing.module.ts
+++ frontend/lupit-times/src/app/app-routing.module.ts
@@ -1,7 +1,49 @@
+import { EditarJogadorComponent } from './editar-jogador/editar-jogador.component';
+import { TimesComponent } from './times/times.component';
+import { CadastroJogadorComponent } from './cadastro-jogador/cadastro-jogador.component';
+import { JogadoresComponent } from './jogadores/jogadores.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroTimeComponent } from './cadastro-time/cadastro-time.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    redirectTo:"jogador",
+    pathMatch:'full'
+  },
+  {
+    path:"jogador",
+    children:
+    [
+      {
+        path:"",
+        component:JogadoresComponent
+      },
+      {
+      path:"novo",
+      component:CadastroJogadorComponent
+      },
+      {
+        path:":id",
+        component: EditarJogadorComponent
+      }
+  ]
+  },
+  {
+    path:"*",
+    pathMatch:"full",
+    component:JogadoresComponent
+  },
+  {
+    path:"Times",
+    component: TimesComponent
+  },
+  {
+    path:"CadastrarTime",
+    component: CadastroTimeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/lupit-times/src/app/app.module.ts
+++ frontend/lupit-times/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule} from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { JogadoresComponent } from './jogadores/jogadores.component';
+import { CadastroJogadorComponent } from './cadastro-jogador/cadastro-jogador.component'
+import { EditarJogadorComponent } from './editar-jogador/editar-jogador.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { CadastroTimeComponent } from './cadastro-time/cadastro-time.component';
+import { TimesComponent } from './times/times.component';
+import { PreloaderComponent } from './shared/components/preloader/preloader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JogadoresComponent,
+    CadastroJogadorComponent,
+    EditarJogadorComponent,
+    HeaderComponent,
+    CadastroTimeComponent,
+    TimesComponent,
+    PreloaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
